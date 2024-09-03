57cb88a042254444e7777d08c62e9d1cbb894827
AmrAbdelhady1 <amr@blazi.co>
2024-09-03 13:18:29 +0300

    .

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 // JSON Server module
 const jsonServer = require("json-server");
 const server = jsonServer.create();
-const router = jsonServer.router("db/db.json");
 const middlewares = jsonServer.defaults();
+const fs = require('fs')
+const path = require('path')
+const filePath = path.join('db.json')
+const data = fs.readFileSync(filePath, "utf-8");
+const db = JSON.parse(data);
+const router = jsonServer.router(db)
 
 server.use(middlewares);
 // Add this before server.use(router)
